6392a34dfff5ead728234bec2933e7fa96a6365b
JhoanGomez30 <jhonan.gomez@genstudents.org>
2024-06-23 21:57:53 -0500

    Ejercicio sucesion de fibonacci

---
+++ app.js
@@ -0,0 +1,37 @@
+const btn_enviar = document.getElementById("button-addon2");
+const contenedor = document.getElementById("response_js")
+
+btn_enviar.addEventListener("click", (e) =>{
+    if (e.target.id === "button-addon2") { 
+        const numeroInput = document.getElementById("input_numero"); 
+        const inputValue = numeroInput.value; 
+
+        if (isNaN(parseInt(inputValue))) {
+            console.log("Ingrese un numero válido");
+        } else {
+            fibonacci(inputValue); 
+        }
+    }
+})
+
+
+const fibonacci = (numero) =>{
+    
+    let a = 0;
+    let b = 1;
+    let c = 0;
+    
+    console.log(a + "\n" + b)
+
+    contenedor.innerHTML = "<p>El numero 1 es: 0</p> "+
+                                "<p>El numero 2 es: 1</p>";
+
+    for(let i = 2; i< numero; i++){
+        c= a+b;
+        a = b;
+        b = c;
+        
+        contenedor.innerHTML+=`<p>El numero ${i+1} es: ${c}</p>`
+    } 
+}
+
